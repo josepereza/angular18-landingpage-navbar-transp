44e65225d929ec9b84aff27d3b86e17c7ebdca89
josepereza <josepereza66@gmail.com>
2024-10-18 09:03:55 +0200

    first commit2

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, HostListener, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular18-landingpage-navbar-transp';
+  isScrolled = signal(false);
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled.set(window.scrollY > 50);
+  }
+}
